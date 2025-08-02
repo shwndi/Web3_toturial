927b82034e210811c00adfa957ebb01080b1d79b
shwndi <924238829@qq.com>
2025-08-02 10:11:41 +0800

    deploy the contract use hardhat network and sepolia

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,5 +2,13 @@ require("@nomicfoundation/hardhat-toolbox");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
+  //defaultNetwork: "hardhat",//默认harthad网络
   solidity: "0.8.28",
+  networks: {
+    sepolia:{
+      //url: Alchemy(alchemy.com) Infura, QulickNode
+      url: "https://eth-sepolia.g.alchemy.com/v2/6bcT10xWViZ-FuWmQe5Sf",
+      accounts:["c1096fd93c110a22efb5d401baaec65c92e36ac462d37e300b0d0a8bb2aabdcc"]
+    }
+  }
 };
--- scripts/FundMe.js
+++ scripts/FundMe.js
@@ -4,3 +4,20 @@
 
 const { ethers } = require("hardhat")
 
+async function main(){
+//create contract
+  const  fundMeFactory  = await ethers.getContractFactory("FundMe")
+  console.log("contract deploying")
+//deploy the contract
+    const fundMe = await fundMeFactory.deploy(10);
+    await fundMe.waitForDeployment();
+    console.log("contract has been deployed successfully, contract address is " + fundMe.target )
+    console.log(`contract has been deployed successfully, contract address is ${fundMe.target}`)
+}
+//main 表示将函数作为参数
+//mian()表示要执行该函数
+main().then().catch((error)=>{
+  console.error(error)
+  //正常结束用0 ，异常结束用1
+  process.exit(1)
+})
